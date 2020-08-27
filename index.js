78a0403cf4726e4ba6d9ba4500508c0d87ff84f8
nattavee <devilzeros@outlook.co.th>
2020-08-27 20:55:45 +0700

    est

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 const PORT = 3456
 
 const app = express()
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(logger('dev'))
