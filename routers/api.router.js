3f7101e7f1edad3a4c01d96da5bad0e4759242fc
Warayut Taekrathok <warayut2555@gmail.com>
2020-09-13 23:52:37 +0700

    Update api.router.js

--- routers/api.router.js
+++ routers/api.router.js
@@ -6,10 +6,6 @@ var app = express()
 
 app.use('/product/', productRoute)
 
-app.use('/', function(req, res) {
-  return apiResponse.successResponse(res, 'Welcome To Sixty Six BBQ API')
-})
-
 // throw 404 if URL not found
 app.all('*', function(req, res) {
   return apiResponse.notFoundResponse(res, 'Page not found')
