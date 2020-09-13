be5a11bbf9c3752e08c081465ac612ce942ccbb0
Warayut Taekrathok <warayut2555@gmail.com>
2020-09-13 23:58:14 +0700

    Update product.model.js

--- models/product.model.js
+++ models/product.model.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const productSchema = new Schema({
-  name:  String,
-  category: String,
-  price: Number,
-  tags: [String]
-}, { timestamps: true, versionKey: false })
+const productSchema = new Schema(
+  {
+    name: String,
+    category: String,
+    price: Number,
+    tags: String
+  },
+  { timestamps: true, versionKey: false }
+)
 
 const ProductModel = mongoose.model('Product', productSchema)
 
